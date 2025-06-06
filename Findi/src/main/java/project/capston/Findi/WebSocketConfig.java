24156524424ec9fa802613fe41c445b5ca2e2dde
DESKTOP-K2GGBTF\jimin <wlalswwkd0@naver.com>
2025-06-06 21:38:52 +0900

    그룹별 채팅방 웹소켓 분기구현 #29

--- Findi/src/main/java/project/capston/Findi/WebSocketConfig.java
+++ Findi/src/main/java/project/capston/Findi/WebSocketConfig.java
@@ -1,7 +1,9 @@
 package project.capston.Findi;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
@@ -10,10 +12,11 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @EnableWebSocket
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketConfigurer{
-    private final ChatHandler chatHandler;
+
+    private final ChatWebSocketHandler chatWebSocketHandler;
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(chatHandler, "/ws/chat")
+        registry.addHandler(chatWebSocketHandler, "/ws/chat/{roomId}")
                 .setAllowedOrigins("*");
     }
 }
