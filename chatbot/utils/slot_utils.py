3c5784511c37ec3354fc69c9087bf75942c7580b
1codus <146055375+1codus@users.noreply.github.com>
2025-06-12 10:17:16 +0900

    챗봇 최종 커밋 #27

--- chatbot/utils/slot_utils.py
+++ chatbot/utils/slot_utils.py
@@ -2,7 +2,6 @@
 from typing import List, Text, Dict, Any
 from rasa_sdk.events import SlotSet, EventType
 
-
 def reset_slots(self) -> List[EventType]:
     return [SlotSet(slot, None) for slot in getattr(self, "slots_to_reset", [])]
 
